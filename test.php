0f849fc9718f6dee682850e9fa282fb8eb1285e2
VukasinStevanovic <vukasin.stevanovic.34.24@ict.edu.rs>
2026-03-28 23:05:27 +0100

    testY

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 <?php
-require_once 'konekcija.php';
-echo 'Konekcija OK';
+try {
+    $pdo = new PDO(
+        'mysql:host=gondola.proxy.rlwy.net;port=31986;dbname=railway;charset=utf8mb4',
+        'root',
+        'gfCjSoYuYgNjwqYVlqfOMyvIebHWYfUg'
+    );
+    echo 'Konekcija OK!';
+} catch (PDOException $e) {
+    echo 'Greška: ' . $e->getMessage();
+}
